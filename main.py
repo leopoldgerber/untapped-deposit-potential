392ecc7aa029b43d9a31ba2ab8ea741432aa3d4d
l.gerber <leopoldgerber@gmail.com>
2024-07-11 12:11:18 +0700

    Added requirements.txt and created main.py with code from recent used notebook

---
+++ main.py
@@ -0,0 +1,16 @@
+'''Main script from notebooks'''
+import pandas as pd
+from notebooks.packages import preprocessing
+
+FILE_PATH = 'data/withdrawal_payment.csv'
+
+df = preprocessing.prepare_dataframe(FILE_PATH)
+df['close_time'] = pd.to_datetime(
+    preprocessing.reverse_date(df['close_time']))
+
+DROP_NAMES = ['LD Rebate', 'Partner Rebate', 'Unknown']
+df = preprocessing.drop_names(df, 'name', DROP_NAMES)
+
+FILE_PATH = preprocessing.get_output()
+FILE_PATH = preprocessing.df_to_xlsx(df, 'df_cutted', FILE_PATH)
+print(FILE_PATH)
