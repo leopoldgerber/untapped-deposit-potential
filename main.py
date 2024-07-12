22a3bd08d48d1a9b0977d175eac9c9a27739b4e9
l.gerber <leopoldgerber@gmail.com>
2024-07-12 10:38:02 +0700

    Added CreateUWP to preprocessing.py inside notebooks/

--- main.py
+++ main.py
@@ -10,12 +10,30 @@ df['close_time'] = pd.to_datetime(
 
 DROP_NAMES = ['LD Rebate', 'Partner Rebate', 'Unknown']
 df = preprocessing.drop_names(df, 'name', DROP_NAMES)
-
+# --- Export
 FILE_PATH = preprocessing.get_output()
 FILE_PATH = preprocessing.df_to_xlsx(df, 'df_cutted', FILE_PATH)
 print(FILE_PATH)
 
+# Load ranger dict and apply range with chosen col for df given
 RANGER_DICT = preprocessing.get_config('ranger.json')
 ranger = preprocessing.CreateRange()
-ranger.create_range(df, 'usd', RANGER_DICT)
-print(df)
+df_ranged = df.copy()
+ranger.apply_range(df_ranged, 'usd', RANGER_DICT)
+# --- Export
+FILE_PATH = preprocessing.get_output()
+FILE_PATH = preprocessing.df_to_xlsx(df_ranged, 'df_ranged', FILE_PATH)
+
+
+RANGER_DICT = preprocessing.get_config('ranger.json')['small']
+START, END, STEP = [x for x in RANGER_DICT.values()]
+
+# Create accumalitive columns in range
+RANGER_DICT = preprocessing.get_config('ranger.json')['small']
+ranger_uwp = preprocessing.CreateUWP()
+df_acc = df.copy()
+usd_list = ranger_uwp.range_columns(df_acc, 'usd', START, END, STEP, 'usd')
+ranger_uwp.range_columns_modify(df_acc, 'usd', usd_list)
+# --- Export
+FILE_PATH = preprocessing.get_output()
+FILE_PATH = preprocessing.df_to_xlsx(df_acc, 'df_acc', FILE_PATH)
