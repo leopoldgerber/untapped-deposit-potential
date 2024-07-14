c9d5ea1b2d109dca591f12662d57809df23578d4
l.gerber <leopoldgerber@gmail.com>
2024-07-14 15:20:53 +0700

    Created components

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 '''Main script from notebooks'''
 import pandas as pd
-from notebooks.packages import preprocessing
+from notebooks.components import prepare
+from notebooks.components import range_ranged
+from notebooks.components import range_accumalative
+from notebooks.components import range_untapped
 
-FILE_PATH = 'data/withdrawal_payment.csv'
-
-df = preprocessing.prepare_dataframe(FILE_PATH)
-df['close_time'] = pd.to_datetime(
-    preprocessing.reverse_date(df['close_time']))
-
-DROP_NAMES = ['LD Rebate', 'Partner Rebate', 'Unknown']
-df = preprocessing.drop_names(df, 'name', DROP_NAMES)
-# --- Export
-FILE_PATH = preprocessing.get_output()
-FILE_PATH = preprocessing.df_to_xlsx(df, 'df_cutted', FILE_PATH)
-print(FILE_PATH)
-
-# Load ranger dict and apply range with chosen col for df given
-RANGER_DICT = preprocessing.get_config('ranger.json')
-ranger = preprocessing.CreateRange()
-df_ranged = df.copy()
-ranger.apply_range(df_ranged, 'usd', RANGER_DICT)
-# --- Export
-FILE_PATH = preprocessing.get_output()
-FILE_PATH = preprocessing.df_to_xlsx(df_ranged, 'df_ranged', FILE_PATH)
+# print(prepare.df)
+# print(range_ranged.df_ranged)
+# print(range_accumalative.df_accumalative)
+print(range_untapped.df_transaction)
 
 
-RANGER_DICT = preprocessing.get_config('ranger.json')['small']
-START, END, STEP = [x for x in RANGER_DICT.values()]
 
-# Create accumalitive columns in range
-RANGER_DICT = preprocessing.get_config('ranger.json')['small']
-ranger_uwp = preprocessing.CreateUWP()
-df_acc = df.copy()
-usd_list = ranger_uwp.range_columns(df_acc, 'usd', START, END, STEP, 'usd')
-ranger_uwp.range_columns_modify(df_acc, 'usd', usd_list)
-# --- Export
-FILE_PATH = preprocessing.get_output()
-FILE_PATH = preprocessing.df_to_xlsx(df_acc, 'df_acc', FILE_PATH)
+# FILE_PATH = 'data/withdrawal_payment.csv'
+
+# df = preprocessing.prepare_dataframe(FILE_PATH)
+# df['close_time'] = pd.to_datetime(
+#     preprocessing.reverse_date(df['close_time']))
+# df['year'] = df['close_time'].dt.year
+# df['month'] = df['close_time'].dt.month
+
+# DROP_NAMES = ['LD Rebate', 'Partner Rebate', 'Unknown']
+# df = preprocessing.drop_names(df, 'name', DROP_NAMES)
+# # --- Export
+# FILE_PATH = preprocessing.get_output()
+# FILE_PATH = preprocessing.df_to_xlsx(df, 'df_cutted', FILE_PATH)
+# print(FILE_PATH)
+
+# # Load ranger dict and apply range with chosen col for df given
+# RANGER_DICT = preprocessing.get_config('ranger.json')
+# ranger = preprocessing.CreateRange()
+# df_ranged = df.copy()
+# ranger.apply_range(df_ranged, 'usd', RANGER_DICT)
+# # --- Export
+# FILE_PATH = preprocessing.get_output()
+# FILE_PATH = preprocessing.df_to_xlsx(df_ranged, 'df_ranged', FILE_PATH)
+
+# # Create accumalitive columns in range
+# RANGER_DICT = preprocessing.get_config('ranger.json')['small']
+# START, END, STEP = [x for x in RANGER_DICT.values()]
+# ranger = preprocessing.CreateRange()
+# df_acc = df.copy()
+# usd_list = ranger.range_columns(df_acc, 'usd', START, END, STEP, 'usd')
+# ranger.range_columns_modify(df_acc, 'usd', usd_list)
+# # --- Export
+# FILE_PATH = preprocessing.get_output()
+# FILE_PATH = preprocessing.df_to_xlsx(df_acc, 'df_acc', FILE_PATH)
